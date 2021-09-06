709f04be62194a749ea59647a5f1158b2bc4ce6a
JavierGold <567498jav@gmail.com>
2021-09-06 13:23:59 -0500

    add: total image activity

--- app/src/main/java/com/example/activities_tarea/TotalImage.kt
+++ app/src/main/java/com/example/activities_tarea/TotalImage.kt
@@ -2,10 +2,27 @@ package com.example.activities_tarea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class TotalImage : AppCompatActivity() {
+    private var imageSource: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_total_image)
+        imageSource = intent.getIntExtra("imageResource", R.drawable.ic_helicopter)
+
+
+        initViews()
+
+        imgTotal.setImageResource(imageSource)
+
+    }
+
+    private lateinit var imgTotal: ImageView
+
+    private fun initViews() {
+        imgTotal = findViewById(R.id.imgTotal)
+
     }
 }
